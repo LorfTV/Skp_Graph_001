b2b1b918ae8c7c0bda475e4e233f231723fe46ee
Nachiketa Vellikad <136580990+LorfTV@users.noreply.github.com>
2024-11-16 14:35:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ st.sidebar.image("skepsis_f(x).png")
 st.sidebar.title("**Graph.F[X]**")  
 st.sidebar.write("*Skepsis Foundation's*") 
 st.sidebar.write("### **Developed By Nachiketa Vellikad**") 
-st.sidebar.write("*Beta_ve--0.1*") 
+st.sidebar.write("*Beta_ve--0.2*") 
 st.sidebar.write("***Write any expression in terms of x and with numerical data, and have fun!***") 
 
 # Input equation and process
